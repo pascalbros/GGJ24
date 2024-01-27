86988f6baadd661275a7c7d086c4cf9a5c39f3d9
Pascal Ambrosini <pasquale.ambrosini@gmail.com>
2024-01-27 15:20:38 +0100

    Updated Player and Guard

---
+++ Assets/Scripts/GuardFieldOfViewController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardFieldOfViewController: MonoBehaviour {
+
+    public float viewRadius = 10f;
+    [Range(0, 360)]
+    public float viewAngle = 90f;
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public Vector3 offset;
+
+    void Update() {
+        DrawFieldOfView();
+    }
+
+    void DrawFieldOfView() {
+        int rayCount = 100;
+        float angleIncrement = viewAngle / rayCount;
+        var position = transform.position + offset;
+
+        for (int i = 0; i <= rayCount; i++) {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + angleIncrement * i;
+            Vector3 dir = Quaternion.Euler(0f, angle, 0f) * transform.forward;
+
+            Ray ray = new(position, dir);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, viewRadius, obstacleMask)) {
+                Debug.DrawLine(position, hit.point, Color.green);
+            } else if (Physics.Raycast(ray, out hit, viewRadius, targetMask)) {
+                Debug.DrawLine(position, hit.point, Color.red);
+            } else {
+                Debug.DrawLine(position, position + dir * viewRadius, Color.green);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class PlayerController: MonoBehaviour {
+
+    public float movementSpeed = 10f;
+    public float rotationSpeed = 9f;
+
+    private CharacterController cc;
+    private Vector2 movementInput;
+
+    void Start() {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        HandleInput();
+    }
+
+    public void Action(InputAction.CallbackContext context) {
+        Debug.Log("Fire!");
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        Debug.Log("Move!");
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    private void HandleInput() {
+        if (movementInput.magnitude == 0f) {
+            //Idle
+        } else {
+            Move(movementInput, movementSpeed);
+        }
+    }
+
+    private void Move(Vector2 movement, float speed) {
+        var input = speed * Time.deltaTime * movement;
+        var inputValue = new Vector3(input.x, 0, input.y);
+        cc.Move(inputValue);
+        transform.DOKill();
+        transform.DORotateQuaternion(Quaternion.LookRotation(inputValue), 0.2f);
+    }
+}
