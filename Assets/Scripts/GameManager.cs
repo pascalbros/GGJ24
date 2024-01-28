c8044ff3bafb8c228daa8e6bdfa0451038056ad6
Pascal Ambrosini <pasquale.ambrosini@gmail.com>
2024-01-28 23:13:38 +0100

    Updated last level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,6 +31,7 @@ public class GameManager: MonoBehaviour {
     }
 
     public void OnPlayerWin() {
+        DisableGuards();
         PlayerController.Instance.OnWin();
         winUI.Appear();
     }
