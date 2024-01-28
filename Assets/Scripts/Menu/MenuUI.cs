8b1a577071163462dd791fb8e20bb544d9069471
Pascal Ambrosini <pasquale.ambrosini@gmail.com>
2024-01-28 14:45:35 +0100

    Added main menu

---
+++ Assets/Scripts/Menu/MenuUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI: MonoBehaviour {
+    public Animator playerAnimator;
+
+    void Start() {
+        playerAnimator.SetFloat("drunkedness", 1f);
+    }
+}
